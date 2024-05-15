6a6c771161eb4bf4eca54c44dbed68e73dd49886
임초희 <hci801140@gmail.com>
2024-05-16 01:00:36 +0900

    Add files via upload

---
+++ 실습/chap06/sec06/One.java
@@ -0,0 +1,11 @@
+package sec06; //p.235-236
+
+public class One {
+	private int secret = 1;
+	int roommate = 2;
+	protected int child = 3;
+	public int anybody = 4;
+
+	public void show() {
+	}
+}
---
+++ 실습/chap06/sec06/other/Three.java
@@ -0,0 +1,13 @@
+package sec06.other; //p.237
+
+import sec06.One;
+
+public class Three {
+	void print() {
+		One o = new One();
+		// System.out.println(o.secret);
+		// System.out.println(o.roommate);
+		// System.out.ptintln(o.child);
+		System.out.println(o.anybody);
+	}
+}
